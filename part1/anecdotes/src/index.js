cbe239d454ded24fa597717e0ee82bbe8be9a0fe
Chrisp <ultrachrisp@gmail.com>
2019-09-05 12:03:10 +0200

    Completed Anecdotes.

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -1,13 +1,60 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
+const Header = ({value}) => <h1>{value}</h1>;
+const Button = ({ onClick, text }) => (
+  <button onClick={onClick}>
+    {text}
+  </button>
+);
+
+const getRandomNum = (len) => Math.floor(Math.random() * len);
+
+const vote = ({current, votes}) => {
+    const copy = [...votes];
+    copy[current] += 1;
+    return copy;
+};
+
+const Statistics = ({selected}) => {    
+    const {votes} = selected,
+          topAnecdoteScore = Math.max(...votes),
+          topAnecdote = votes.indexOf(topAnecdoteScore);
+    
+    if(topAnecdoteScore <= 0){
+        return (
+            <>
+                <Header value='Anecdote with the most votes'/>
+                <p>No votes yet</p>
+            </>
+        );
+    }
+    
+    return (
+        <>
+            <Header value='Anecdote with the most votes'/>
+            <div>{anecdotes[topAnecdote]}</div>
+            <div>has {topAnecdoteScore} votes</div>
+        </>
+    );
+};
+
 const App = (props) => {
-    const [selected, setSelected] = useState(0);
+    const [selected, setSelected] = useState({
+        current: getRandomNum(props.anecdotes.length),
+        votes: [...Array(props.anecdotes.length)].map(() => 0)});
+
+    const handleNextAnecdote = () => setSelected({...selected, current: getRandomNum(props.anecdotes.length)});
+    const handleVote = () => setSelected({...selected, votes: vote(selected)});
 
     return (
-        <div>
-            {props.anecdotes[selected]}
-        </div>
+        <>
+            <Header value='Anecdote of the day' />
+            <p>{props.anecdotes[selected.current]}</p>
+            <Button onClick={handleVote} text='vote' />
+            <Button onClick={handleNextAnecdote} text='next anecdote' />
+            <Statistics selected={selected} />
+        </>
     );
 };
 

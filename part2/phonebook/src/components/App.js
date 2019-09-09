4532ad100daf11b71dc5028e0ec25de7e1a7ca6b
Chrisp <ultrachrisp@gmail.com>
2019-09-09 11:47:38 +0200

    2.15 & 2.16 are done.

--- part2/phonebook/src/components/App.js
+++ part2/phonebook/src/components/App.js
@@ -1,19 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Filter from './Filter';
 import PersonForm from './PersonForm';
 import Persons from './Persons';
+import personsService from '../service/persons';
 
 const App = () => {
-    const [ persons, setPersons] = useState([
-        { name: 'Arto Hellas', number: '040-123456' },
-        { name: 'Ada Lovelace', number: '39-44-5323523' },
-        { name: 'Dan Abramov', number: '12-43-234345' },
-        { name: 'Mary Poppendieck', number: '39-23-6423122' }
-    ]);
+    const [ persons, setPersons] = useState([]);
     const [ newName, setNewName ] = useState('');
     const [ newNumber, setNewNumber ] = useState('');
     const [ filter, setFilter ] = useState('');
 
+    const hook = () => {
+        personsService
+            .getAll()
+            .then(initialPersons => {
+                setPersons(initialPersons);
+            });
+    };
+
+    useEffect(hook, []);
+    
     const addName = (evt) => {
         evt.preventDefault();
         

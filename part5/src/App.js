54c70bf6277ce97cf36ce767c7b963ac85266049
Chrisp <ultrachrisp@gmail.com>
2019-10-23 13:23:11 +0200

    Adding front end.

--- part5/src/App.js
+++ part5/src/App.js
@@ -1,14 +1,30 @@
 import React, { useState, useEffect } from 'react';
 import Blog from './components/Blog';
+import loginService from './services/login';
 import blogService from './services/blogs';
 import './App.css';
 
 function App() {
+  const [errorMessage, setErrorMessage] = useState(null);
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [user, setUser] = useState(null);
+  const [loginVisible, setLoginVisible] = useState(false);
 
-  const handleLogin = (evt) => {
+  const handleLogin = async (evt) => {
     evt.preventDefault();
+    try {
+      const user = await loginService.login({ username, password });
+
+      window.localStorage.setItem('loggedBlogUser', JSON.stringify(user));
+      blogService.setToken(user.token);
+      setUser(user);
+      setUsername('');
+      setPassword('');
+    }catch(exception){
+      setErrorMessage('Wrong credentials');
+      setTimeOut(() => setErrorMessage(null), 5000);
+    }
   };
   
   return (

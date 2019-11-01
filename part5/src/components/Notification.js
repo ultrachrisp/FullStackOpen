90772352f47dff43d34f8922def804a01d0d5903
Chrisp <ultrachrisp@gmail.com>
2019-11-01 12:50:58 +0200

    Coloured notifications, finished 5.5

--- part5/src/components/Notification.js
+++ part5/src/components/Notification.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const StyledNotification = styled.div`
+background-color: #ccc;
+padding: 20px;
+border: 3px solid ${props => props.type};
+`;
 
 const Notification = ({msg}) => {
   const {message, type} = msg;
   if(!message){ return null; }
 
+  const notificationColor = (type === 'status'? 'green' : 'red');
+  
   return (
-    <div className={type}>
+    <StyledNotification type={(type)? notificationColor: false}>
       {message}
-    </div>
+    </StyledNotification>
   );
 };
 

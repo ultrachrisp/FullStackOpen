4044868748a8e73127a152e49d1cfda35f156f4d
Chrisp <ultrachrisp@gmail.com>
2019-11-05 11:52:02 +0200

    Showing blog posts on click.

--- part5/src/components/Blog.js
+++ part5/src/components/Blog.js
@@ -1,8 +1,33 @@
-import React from 'react';
-const Blog = ({ blog, onClick }) => (
-  <div>
-    {blog.title} {blog.author} <button name={blog.id} onClick={onClick}>Delete</button>
-  </div>
-);
+import React, { useState }from 'react';
+import styled from 'styled-components';
+
+const StyledBlog = styled.div`
+border: 1px solid #000;
+display: block;
+margin-bottom: 10px;
+padding: 10px;
+`;
+
+const Blog = ({ blog, onClick }) => {
+  const [visible, setVisible] = useState(false);
+  
+  const hideWhenVisible = { display: visible ? 'none' : '' },
+        showWhenVisible = { display: visible ? '' : 'none' },
+        toggleVisibility = () => { setVisible(!visible); },
+        { title, author, url, likes, user } = blog;
+  
+  return (
+    <StyledBlog>
+      <div onClick={() => toggleVisibility() }>
+        { title } by { author }
+      </div>
+      <div style={ showWhenVisible }>
+        <a href={ url }>{ url }</a>
+        <div>{likes}<button onClick={()=>{}}>Like</button></div>
+        <div>Added by { user[0].username }</div>
+        <button name={blog.id} onClick={onClick}>Delete</button>
+      </div>
+    </StyledBlog>
+  );};
 
 export default Blog;
--- part5/src/components/Togglable.js
+++ part5/src/components/Togglable.js
@@ -3,19 +3,20 @@ import React, { useState, useImperativeHandle } from 'react';
 const Togglable = React.forwardRef((props, ref) => {
   const [visible, setVisible] = useState(false);
 
-  const hideWhenVisible = { display: visible ? 'none' : '' };
-  const showWhenVisible = { display: visible ? '' : 'none' };
-  const toggleVisibility = () => { setVisible(!visible); };
+  const hideWhenVisible = { display: visible ? 'none' : '' },
+        showWhenVisible = { display: visible ? '' : 'none' },
+        toggleVisibility = () => { setVisible(!visible); };
+  
   useImperativeHandle(ref, () => {return { toggleVisibility }; });
 
   return (
     <>
       <div style={ hideWhenVisible }>
-        <button onClick={() => toggleVisibility(true)}>{ props.buttonLabel }</button>
+        <button onClick={() => toggleVisibility() }>{ props.buttonLabel }</button>
       </div>
       <div style={ showWhenVisible }>
         { props.children }
-        <button onClick={() => toggleVisibility(false)}>cancel</button>
+        <button onClick={() => toggleVisibility() }>cancel</button>
       </div>
     </>
   );

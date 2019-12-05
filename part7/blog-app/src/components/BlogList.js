5a2f39231276cd2989f6759f2920cf6a7c663383
Chrisp <ultrachrisp@gmail.com>
2019-12-05 10:44:43 +0200

    Start of adding react-router.

---
+++ part7/blog-app/src/components/BlogList.js
@@ -0,0 +1,107 @@
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+
+import Blog from './Blog';
+import BlogForm from './BlogForm';
+import LoginForm from './LoginForm';
+import Notification from './Notification';
+import { useField } from '../hooks/index';
+
+import { initialiseBlogs, removeBlog, voteFor } from '../reducers/blogsReducer';
+import { logIn, logOut, checkLogin } from '../reducers/usersReducer';
+
+const BlogList = (props) => {
+  const username = useField('text');
+  const password = useField('password');
+
+  useEffect(() => {
+    props.initialiseBlogs();
+    props.checkLogin();
+  },[]);
+
+  const handleLogin = (evt) => {
+    evt.preventDefault();
+    props.logIn({
+      username: username.value,
+      password: password.value
+    });
+    username.onSubmit();
+    password.onSubmit();
+  };
+
+  const handleLogout = () => {
+    props.logOut();
+  };
+
+  const handleDelete = (evt) => {
+    if(window.confirm('Remove blog')) {
+      props.removeBlog(evt.target.name);
+    }
+  };
+
+  const handleLike = (evt) => {
+    const blog = props.blogs.find(elem => elem.id === evt.target.name);
+    blog.likes++;
+
+    props.voteFor(blog);
+  };
+
+  const loginForm = () => {
+    return (
+      <LoginForm
+        username={ username }
+        password={ password }
+        handleSubmit={ handleLogin } />
+    );
+  };
+
+  const displayBlogs = () => {
+    return (
+      <>
+        <p>{props.user.name} logged in
+          <button onClick={ handleLogout }>logout</button>
+        </p>
+        <BlogForm />
+        <h2>blogs</h2>
+        {props.sortedByLikes.map(blog =>
+                                 <Blog
+                                   key={ blog.id }
+                                   blog={ blog }
+                                   currentUser={ props.user.username }
+                                   onLike={ handleLike }
+                                   onDelete={ handleDelete }/> )}
+      </>
+    );
+  };
+
+  return (
+    <>
+      <h1>Blogs</h1>
+      <Notification />
+      { props.user === null? loginForm() : displayBlogs() }
+    </>
+  );
+};
+
+const mapStateToProps = (state) => {
+  const sortedByLikes = state.blogs.sort((a, b) => a.likes < b.likes);
+  return {
+    sortedByLikes,
+    blogs: state.blogs,
+    user: state.user
+  };
+};
+
+const mapDispatchToProps = {
+  initialiseBlogs,
+  removeBlog,
+  voteFor,
+  logIn,
+  logOut,
+  checkLogin
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(BlogList);

685ce8911ff55ffe0a2ae09ee5d86ce63d214bcd
Chrisp <ultrachrisp@gmail.com>
2019-12-12 12:52:30 +0200

    Displaying users & their blogs.

--- part7/blog-app/src/components/User.js
+++ part7/blog-app/src/components/User.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { useParams } from 'react-router-dom';
 
 const User = (props) => {
-  return (
-    <>
-      <div>Some stuffs</div>
-    </>
-  );
+  const { id } = useParams();
+  const userBlogs = props.blogs.filter(blog => blog.user[0].id === id);
+
+  return (!userBlogs[0])?
+    null:
+    (
+      <>
+        <h2>{userBlogs[0].user[0].username}</h2>
+        <ul>
+          { userBlogs.map(blog => <li key={ blog.id }>{ blog.title }</li>) }
+        </ul>
+      </>
+    );
 };
 const mapStateToProps = (state, ownProps) => {
-  const userBlogs = state.blogs.filter(blog => console.log(blog));
   return {
-    blogs: userBlogs
+    blogs: state.blogs
   };
 };
 

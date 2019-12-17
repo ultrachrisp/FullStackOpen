f6ce1de5030e7ab003dc9e5f15dab8bbb440f79c
Chrisp <ultrachrisp@gmail.com>
2019-12-17 12:06:37 +0200

    Start of the menu.

--- part7/blog-app/src/components/App.js
+++ part7/blog-app/src/components/App.js
@@ -1,74 +1,15 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-
-import User from './User';
-import Blog from './Blog';
-import UserList from './UserList';
-import BlogList from './BlogList';
-import LoginForm from './LoginForm';
+import Menu from './Menu';
 import Notification from './Notification';
-import { useField } from '../hooks/index';
-import { logIn, logOut, checkLogin } from '../reducers/usersReducer';
 
 const App = (props) => {
-  const username = useField('text');
-  const password = useField('password');
-
-  useEffect(() => {
-    props.checkLogin();
-  },[]);
-
-  const handleLogin = (evt) => {
-    evt.preventDefault();
-    props.logIn({
-      username: username.value,
-      password: password.value
-    });
-    username.onSubmit();
-    password.onSubmit();
-  };
-
-  const handleLogout = () => {
-    props.logOut();
-  };
-
-  const loginForm = () => {
-    return (
-      <LoginForm
-        username={ username }
-        password={ password }
-        handleSubmit={ handleLogin } />
-    );
-  };
-
-  const UserStatus = () => {
-    return (
-      <p>
-        { props.user.name } logged in
-        <button onClick={ handleLogout }>logout</button>
-      </p>
-    );
-  };
-
-  const content = props.user === null?
-        loginForm():
-        (
-          <Router>
-            <UserStatus />
-            <Route exact path="/" render={() => <BlogList /> }/>
-            <Route exact path="/users" render={() => <UserList /> }/>
-            <Route path="/users/:id" render={() => <User/> }/>
-            <Route path="/blogs/:id" render={() => <Blog/> }/>
-          </Router>
-        );
-  
   return (
     <>
+      <Menu />
       <h1>Keep track of useful blogs</h1>
       <Notification />
-      { content }
     </>
   );
 };
@@ -79,13 +20,7 @@ const mapStateToProps = (state) => {
   };
 };
 
-const mapDispatchToProps = {
-  logIn,
-  logOut,
-  checkLogin
-};
-
 export default connect(
   mapStateToProps,
-  mapDispatchToProps
+  null
 )(App);
---
+++ part7/blog-app/src/components/Menu.js
@@ -0,0 +1,85 @@
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import User from './User';
+import Blog from './Blog';
+import UserList from './UserList';
+import BlogList from './BlogList';
+import LoginForm from './LoginForm';
+import { useField } from '../hooks/index';
+import { logIn, logOut, checkLogin } from '../reducers/usersReducer';
+
+const Menu = (props) => {
+  const username = useField('text');
+  const password = useField('password');
+
+  useEffect(() => {
+    props.checkLogin();
+  },[]);
+
+  const handleLogin = (evt) => {
+    evt.preventDefault();
+    props.logIn({
+      username: username.value,
+      password: password.value
+    });
+    username.onSubmit();
+    password.onSubmit();
+  };
+
+  const handleLogout = () => {
+    props.logOut();
+  };
+
+  const loginForm = () => {
+    return (
+      <LoginForm
+        username={ username }
+        password={ password }
+        handleSubmit={ handleLogin } />
+    );
+  };
+
+  const UserStatus = () => {
+    return (
+      <p>
+        { props.user.name } logged in
+        <button onClick={ handleLogout }>logout</button>
+      </p>
+    );
+  };
+
+  const content = props.user === null?
+        loginForm():
+        (
+          <Router>
+            <UserStatus />
+            <Route exact path="/" render={() => <BlogList /> }/>
+            <Route exact path="/users" render={() => <UserList /> }/>
+            <Route path="/users/:id" render={() => <User/> }/>
+            <Route path="/blogs/:id" render={() => <Blog/> }/>
+          </Router>
+        );
+  
+  return(
+    <div>Menu</div>
+  );
+};
+
+const mapStateToProps = (state) => {
+  return {
+    user: state.user
+  };
+};
+
+const mapDispatchToProps = {
+  logIn,
+  logOut,
+  checkLogin
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Menu);
